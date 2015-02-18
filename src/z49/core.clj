33700a9148c77a366d30a8b89cea4eb64e1bf5ce
Clouds <Clouds.Flowing@gmail.com>
2015-02-19 01:07:27 +0800

    basic layout

--- src/z49/core.clj
+++ src/z49/core.clj
@@ -1,11 +1,31 @@
 (ns z49.core
-	(:require [clj-http.client :as client])
+    (:require [clj-http.client :as client])
     (:use seesaw.core))
+
+(native!)
+
+(def input-box
+  (text))
+
+(def tab
+  (top-bottom-split
+    "view"
+    input-box
+    :divider-location 2/3))
+
+(def layout-main
+  (left-right-split
+    "tabs"
+    tab
+    :divider-location 1/3))
+
+(def mainframe (frame :title "z49"
+                      :size [300 :by 200]
+                      :content layout-main
+                      :on-close :exit))
 
 (defn -main [& args]
   (invoke-later
-    (-> (frame :title "Hello",
-           :content "Hello, Seesaw",
-           :on-close :exit)
-     pack!
-     show!)))
+   (-> mainframe
+       ;pack!
+       show!)))
